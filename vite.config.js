bc9978a0e1adee215084acf084e169a50a8c17a5
Jonathan Ruiz Silva <ruizsilvajonathan@gmail.com>
2025-11-27 11:33:23 -0300

    fixed paths error on vite preview

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
-export default defineConfig({
-  base: "/js-ejercicios-1",
-});
+export default defineConfig(({ command }) => ({
+  // base: command === "build" ? "/js-ejercicios-1/" : "/",
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, "index.html"),
+        ejercicio1: resolve(__dirname, "ejercicio1.html"),
+        ejercicio2: resolve(__dirname, "ejercicio2.html"),
+      },
+    },
+  },
+}));
