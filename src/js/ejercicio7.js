89bf017fd776dbe11d9d777ad89789bbbd56cc94
Jonathan Ruiz Silva <ruizsilvajonathan@gmail.com>
2025-12-05 02:02:47 -0300

    password generator done

--- src/js/ejercicio7.js
+++ src/js/ejercicio7.js
@@ -2,10 +2,21 @@ import { generatePassword } from "../utils/generatePassword";
 
 const passwordGenerated = document.getElementById("passwordGenerated");
 const formGeneratePassword = document.getElementById("formGeneratePassword");
+const passwordLengthInput = document.getElementById("passwordLength");
 
 formGeneratePassword.addEventListener("submit", (e) => {
   e.preventDefault();
-  const passwordLength = document.getElementById("passwordLength").value;
+  const passwordLength = passwordLengthInput.value;
+  if (passwordLength === "") {
+    passwordGenerated.textContent = "La longitud es requerida";
+    return;
+  }
+
+  if (passwordLength < 4) {
+    passwordGenerated.textContent =
+      "La longitud de la contraseña debe ser mayor o igual a 4";
+    return;
+  }
 
   const password = generatePassword(passwordLength);
 
--- src/utils/generatePassword.js
+++ src/utils/generatePassword.js
@@ -1,3 +1,22 @@
-export const generatePassword = () => {
-  return "test";
+export const generatePassword = (longitud = 4) => {
+  const mayusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const minusculas = "abcdefghijklmnopqrstuvwxyz";
+  const numeros = "0123456789";
+  const caracteres = "!\"'#$%&()*+,-./:;<=>?@[]^_`{|}~";
+
+  const letras = mayusculas + minusculas;
+  const caracteresDisponibles = [letras, numeros, caracteres];
+  let password = "";
+
+  for (let i = 0; i < longitud; i++) {
+    const randomIndex = Math.floor(
+      Math.random() * caracteresDisponibles.length
+    );
+    const randomChar = Math.floor(
+      Math.random() * caracteresDisponibles[randomIndex].length
+    );
+    password += caracteresDisponibles[randomIndex][randomChar];
+  }
+
+  return password;
 };
